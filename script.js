7ba8501a62eafe27eb57c2dd7b6e186abe327616
Lordmanceroot <84021061+Lordmanceroot@users.noreply.github.com>
2021-11-06 13:10:55 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const  content = canvas.getContext('2d');
+const content = canvas.getContext('2d');
 
 const bird = new Image();
 const bg = new Image();
@@ -8,7 +8,7 @@ const pipeUp = new Image();
 const pipeBottom = new Image();
 
 const fly = new Audio();
-const score = new Audio();
+const scoreAudio = new Audio();
 
 bird.src = "img/bird.png";
 bg.src = "img/bg.png";
@@ -16,11 +16,14 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png";
 pipeBottom.src = "img/pipeBottom.png";
 fly.src = "audio/fly.mp3";
-score.src = "audio/score.mp3";
+scoreAudio.src = "audio/score.mp3";
 
 // Позиция птички 
 const xPos = 10;
-      yPos = 200;
+let yPos = 150;
+let grav = 1.5;
+
+let score = 0;
 
 // Расстояние между трубами 
 const gap = 90;
@@ -29,30 +32,55 @@ const gap = 90;
 document.addEventListener('keydown', moveUp);
 
 function moveUp() {
-    yPos -= 30;
+    yPos -= 25;
+    fly.play();
 }
 
 // Создание блоков 
 const pipe = [];
 pipe[0] = {
-    x : canvas.width,
-    y : 0
+    x: canvas.width,
+    y: 0
 };
 
 // Прорисовка 
 function draw() {
-    content.drawImage(bg, 0 , 0);
+    content.drawImage(bg, 0, 0);
 
-    for(var i = 0; i < pipe.length; i++) {
+    for (let i = 0; i < pipe.length; i++) {
         content.drawImage(pipeUp, pipe[i].x, pipe[i].y);
         content.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
         pipe[i].x--;
+
+        if (pipe[i].x === 125) {
+            pipe.push({
+                x: canvas.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+
+        if(xPos + bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height
+                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= canvas.height - fg.height) {
+            location.reload(); // Перезагрузка страницы
+        }
+
+        if(pipe[i].x === 5) {
+            score++;
+            scoreAudio.play();
+        }
     }
     content.drawImage(bird, xPos, yPos);
     content.drawImage(fg, 0, canvas.height - fg.height);
 
-    yPos += 1;
+    yPos += grav;
+
+    content.fillStyle = "#000";
+    content.font = "24px Verdana";
+    content.fillText("Счет: " + score, 10, canvas.height - 20);
+
     requestAnimationFrame(draw);
 }
 
